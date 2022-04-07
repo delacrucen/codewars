0234aa88a6a43bc15e231a76f518fc130372edc6
Mauricio De La Cruz <delacruzen@hotmail.com>
2022-04-07 18:04:56 -0400

    Add solution

---
+++ 8kyu/return-phrase-from-array.js
@@ -0,0 +1,26 @@
+//Problem
+/*Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+I love you
+a little
+a lot
+passionately
+madly
+not at all
+When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.*/
+
+// Program
+function howMuchILoveYou(nbPetals) {
+  const phrase = ["I love you", "a little", "a lot", "passionately", "madly", "not at all"];
+  return nbPetals <= 0 ? "Please insert positive numbers" : phrase[(nbPetals - 1) % phrase.length];
+}
+
+//Console testing
+console.log(howMuchILoveYou(19));
+console.log(howMuchILoveYou(0));
+
+//Expected output
+//"I love you"
+//"Please insert positive numbers"
