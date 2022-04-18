325846ae853575e874d778adf36ae91e93dbcd92
Mauricio De La Cruz <58452509+delacrucen@users.noreply.github.com>
2022-04-18 18:51:18 -0400

    Add solution

---
+++ 8kyu/return-number-to-string-fundamentals.js
@@ -0,0 +1,16 @@
+// Problem
+/*We need a functiont that can transform a number into a string.
+Examples:
+123 --> "123"
+999 --> "999"*/
+
+// Program
+function numberToString(num){
+	return String(num);
+};
+
+// Console testing
+console.log(numberToString(543));
+
+// Expected output
+//"543"
