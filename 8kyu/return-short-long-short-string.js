5d758c6f7acb48095800e44b19766283c0786e36
Mauricio De La Cruz <58452509+delacrucen@users.noreply.github.com>
2022-04-19 12:25:33 -0400

    Add solution

---
+++ 8kyu/return-short-long-short-string.js
@@ -0,0 +1,14 @@
+//Problem
+
+/*Given 2 strings, a and b, return a string of the form short+long+short, with  the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty (zero length).*/
+
+// Program
+function solution(a, b){
+	return a.length > b.length ? b + a + b : a + b + a;
+}
+
+// Console testing
+console.log(solution("54", "1"));
+
+// Expected output
+//"1541"
