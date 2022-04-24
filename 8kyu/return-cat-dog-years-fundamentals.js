39f6ad777b49aa61bebc42ba310bac3c5229a91e
Mauricio De La Cruz <58452509+delacrucen@users.noreply.github.com>
2022-04-24 11:04:26 -0400

    Add solution

---
+++ 8kyu/return-cat-dog-years-fundamentals.js
@@ -0,0 +1,47 @@
+//Problem
+/*I have a cat and a dog.
+
+I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+Return their respective ages now as [humanYears,catYears,dogYears]
+
+NOTES:
+
+humanYears >= 1
+humanYears are whole numbers only
+
+Cat Years
+15 cat years for first year
++9 cat years for second year
++4 cat years for each year after that
+
+Dog Years
+15 dog years for first year
++9 dog years for second year
++5 dog years for each year after that*/
+
+// Program
+var humanYearsCatYearsDogYears = function(humanYears) {
+  // first year
+  if(humanYears === 1){
+    return [humanYears, 15, 15];
+  }
+  // second year
+  if(humanYears === 2){
+    return [humanYears, 24, 24];
+  }
+  // +3 years
+  if(humanYears > 2){
+    return [humanYears, 24 + ((humanYears - 2) * 4), 24 + (humanYears - 2) * 5];
+  }
+}
+
+//Console testing
+console.log(humanYearsCatYearsDogYears(12));
+console.log(humanYearsCatYearsDogYears(42));
+console.log(humanYearsCatYearsDogYears(37));
+
+//Expected output
+// [ 12, 64, 74 ]
+// [ 42, 184, 224 ]
+// [ 37, 164, 199 ]
