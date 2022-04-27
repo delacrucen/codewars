0480eec8128e6750bbca9360d4949ecdad568dca
Mauricio De La Cruz <58452509+delacrucen@users.noreply.github.com>
2022-04-27 12:17:59 -0400

    Add solution

---
+++ 8kyu/return-two-numbers-multiplication-fundamentals.js
@@ -0,0 +1,15 @@
+//Problem
+/*Implement a function which multiplies two numbers.*/
+
+// Program
+function multiply(a, b){
+  return a * b;
+}
+
+//Console testing
+console.log(multiply(5, 5))
+console.log(multiply(4, 2))
+
+//Expected output
+// 25
+// 8
