357e303fdedf317f5d89754b2ce42e975106eb40
Mauricio De La Cruz <58452509+delacrucen@users.noreply.github.com>
2022-05-03 06:21:09 -0400

    Add solution

---
+++ 8kyu/return-user-greetings.js
@@ -0,0 +1,13 @@
+// Problem
+/*Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".*/
+
+// Program
+function greet(name){
+  return `Hello, ${name} how are you doing today?`
+}
+
+// Console testing
+console.log(greet("Mauri"));
+
+// Expected output
+//"Hello, Mauri how are you doing today?"
