d2cf733dffd6b343abf68b891ea070516fa9efcf
Mauricio De La Cruz <58452509+delacrucen@users.noreply.github.com>
2022-05-05 17:16:40 -0400

    Add solution

---
+++ 8kyu/return-username-confirmation-fundamentals.js
@@ -0,0 +1,22 @@
+// Problem
+/*Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+The function takes a name as its only argument, and returns one of the following strings:
+
+name + " plays banjo" 
+name + " does not play banjo"
+Names given are always valid strings.*/
+
+// Program
+function areYouPlayingBanjo(name) {
+  return (name[0] === "R" || name[0] === "r") ? `${name} plays banjo` : `${name} does not play banjo`;
+}
+
+// Console testing
+console.log(areYouPlayingBanjo("Mauri"))
+console.log(areYouPlayingBanjo("Roger"))
+
+// Expected output
+// Mauri does not play banjo
+// Roger plays banjo
