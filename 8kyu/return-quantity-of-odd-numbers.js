645f4f816a0bef58523f44bf0619407eca716161
Mauricio De La Cruz <58452509+delacrucen@users.noreply.github.com>
2022-05-07 14:38:25 -0400

    Add solution

---
+++ 8kyu/return-quantity-of-odd-numbers.js
@@ -0,0 +1,20 @@
+// Problem
+/* Given a number n, return the number of positive odd numbers below n, EASY!
+
+Examples (Input -> Output)
+* 7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+* 15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+Expect large Inputs! */
+
+// Program
+function oddCount(n){
+  return Math.floor(n/2);
+}
+
+// Console testing
+console.log(oddCount(32))
+console.log(oddCount(50))
+
+// Expected output
+// 16
+// 25
