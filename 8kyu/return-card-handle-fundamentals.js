573dd1363ddc0097fe16a436f7cb20e54b47566a
Mauricio De La Cruz <58452509+delacrucen@users.noreply.github.com>
2022-05-14 18:26:28 -0400

    Add solution

---
+++ 8kyu/return-card-handle-fundamentals.js
@@ -0,0 +1,27 @@
+// Problem
+/*You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+Our deck (is preloaded):
+
+('3♣') -> return 'clubs'
+('3♦') -> return 'diamonds'
+('3♥') -> return 'hearts'
+('3♠') -> return 'spades'*/
+
+// Program
+function defineSuit(card) {
+  if(card.includes("♥")) return "hearts"
+  if(card.includes("♦")) return "diamonds"
+  if(card.includes("♣")) return "clubs"
+  if(card.includes("♠")) return "spades" 
+}
+
+// Console testing
+console.log(defineSuit("5♠"));
+console.log(defineSuit("8♣"));
+console.log(defineSuit("10♦"));
+
+// Expected output
+// spades
+// clubs
+// diamonds
