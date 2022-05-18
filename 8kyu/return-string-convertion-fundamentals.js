9ad7fdb53f2ce91753b5a92b573d57a95e822187
Mauricio De La Cruz <58452509+delacrucen@users.noreply.github.com>
2022-05-18 17:27:24 -0400

    Add solution

---
+++ 8kyu/return-string-convertion-fundamentals.js
@@ -0,0 +1,16 @@
+// Problem
+/*Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.*/
+
+// Program
+function booleanToString(b){
+  return `${b}`;
+}
+// Console testing
+console.log(booleanToString(false));
+console.log(booleanToString(true));
+
+// Expected output
+// "false"
+// "true"
