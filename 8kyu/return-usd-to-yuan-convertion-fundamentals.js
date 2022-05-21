e9bf263f638b02a88ca6c3057c0852ae3f7d2185
Mauricio De La Cruz <58452509+delacrucen@users.noreply.github.com>
2022-05-21 10:12:38 -0400

    Add solution

---
+++ 8kyu/return-usd-to-yuan-convertion-fundamentals.js
@@ -0,0 +1,20 @@
+//Problem
+/*Create a function that converts US dollars (USD) to Chinese Yuan (CNY) . The input is the amount of USD as an integer, and the output should be a string that states the amount of Yuan followed by 'Chinese Yuan'
+
+Examples (Input -> Output)
+* 15  -> '101.25 Chinese Yuan'
+* 465 -> '3138.75 Chinese Yuan'
+The conversion rate you should use is 6.75 CNY for every 1 USD. All numbers should be represented as a string with 2 decimal places. (e.g. "21.00" NOT "21.0" or "21")*/
+
+// Program
+function usdcny(usd) {
+  return `${(usd * 6.75).toFixed(2)} Chinese Yuan`
+}
+
+//Console testing
+console.log(usdcny(60));
+console.log(usdcny(15));
+
+//Expected output
+// "405.00 Chinese Yuan"
+// "101.25 Chinese Yuan"
