4ecd1a72a09b07054f2ad98bead6864d15a8485d
Mauricio De La Cruz <58452509+delacrucen@users.noreply.github.com>
2022-05-25 13:34:25 -0400

    Add solution

---
+++ 8kyu/return-doubled-input.js
@@ -0,0 +1,13 @@
+//Problem
+/*Code as fast as you can! You need to double the integer and return it.*/
+
+// Program
+function doubleInteger(i) {
+  return i * 2;
+}
+
+//Console testing
+console.log(doubleInteger(5));
+
+//Expected output
+//10
