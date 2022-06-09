67dc14a27b5ac88c1b092232fd4e3cd5edd4cb99
Mauricio De La Cruz <58452509+delacrucen@users.noreply.github.com>
2022-06-09 16:16:20 -0400

    Add solution

---
+++ 8kyu/return-one-or-more-strings-together-fundamentals.js
@@ -0,0 +1,19 @@
+// Problem
+/*Create a method sayHello/say_hello/SayHello that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
+
+Example:
+
+sayHello(['John', 'Smith'], 'Phoenix', 'Arizona')
+This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!*/
+
+// Program
+function sayHello(name, city, state) {
+  let fullName = name.join(" ");
+  return `Hello, ${fullName}! Welcome to ${city}, ${state}!` ;
+}
+
+// Console testing
+console.log(sayHello(["John", "Smith", "Barnes"], "Roma", "Italy"))
+
+// Expected output
+// "Hello, John Smith Barnes! Welcome to Roma, Italy!"
