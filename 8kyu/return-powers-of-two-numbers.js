5840ab348dc72ba91c57b82d597e909bdb208063
Mauricio De La Cruz <58452509+delacrucen@users.noreply.github.com>
2022-06-14 14:08:49 -0400

    Add solution

---
+++ 8kyu/return-powers-of-two-numbers.js
@@ -0,0 +1,23 @@
+// Problem
+/*Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+Examples
+n = 0  ==> [1]        # [2^0]
+n = 1  ==> [1, 2]     # [2^0, 2^1]
+n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]*/
+
+// Program
+function powersOfTwo(n){
+  let result = [];
+  for(let i=0; i<=n; i++){
+    result.push(Math.pow(2, i));
+  }
+  return result;
+}
+
+
+// Console testing
+console.log(powersOfTwo(10))
+
+// Expected output
+// 2
