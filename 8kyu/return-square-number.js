e014a558a1e1f30341c32e787137a17a15f6720a
Mauricio De La Cruz <58452509+delacrucen@users.noreply.github.com>
2022-06-18 09:18:01 -0400

    Add solution

---
+++ 8kyu/return-square-number.js
@@ -0,0 +1,13 @@
+// Problem
+/*Now you have to write a function that takes an argument and returns the square of it.*/
+
+// Program
+const square = num => Math.pow(num, 2);
+
+// Console testing
+console.log(square(3));
+console.log(square(4));
+
+// Expected output
+// 9
+// 16
