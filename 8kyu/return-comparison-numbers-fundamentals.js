e14a6e20382a63d9f755ba427b895b75551d91dc
Mauricio De La Cruz <58452509+delacrucen@users.noreply.github.com>
2022-07-02 16:27:06 -0400

    Add solution

---
+++ 8kyu/return-comparison-numbers-fundamentals.js
@@ -0,0 +1,40 @@
+// Problem
+/*
+Create a function close_compare that accepts 3 parameters: a, b, and an optional margin. The function should return whether a is lower than, close to, or higher than b.
+
+a is considered "close to" b if margin is greater than or equal to the distance between a and b.
+
+Please note the following:
+
+When a is close to b, return 0.
+Otherwise...
+
+When a is less than b, return -1.
+
+When a is greater than b, return 1.
+
+If margin is not given, treat it as zero.
+
+Assume: margin >= 0
+*/
+
+//Program
+function closeCompare(a, b, margin = 0) {
+  if (a < b - margin){
+    return -1;
+  } 
+  if (a - margin > b){
+    return 1;
+  } 
+  return 0;
+}
+
+// Console testing
+console.log(closeCompare(2, 5, 3));
+console.log(closeCompare(8.1, 5, 3))
+console.log(closeCompare(1.99, 5, 3))
+    
+// Expected output
+// 0
+// 1
+// -1
