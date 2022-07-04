0f1a555b875dd89327d8a6cbf15a76c0aad5b1ab
Mauricio De La Cruz <58452509+delacrucen@users.noreply.github.com>
2022-07-04 14:11:45 -0400

    Add solution

---
+++ 8kyu/return-sum-of-results-array.js
@@ -0,0 +1,36 @@
+// Problem
+/*
+Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+
+For example: ["3:1", "2:2", "0:1", ...]
+
+Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+
+if x > y: 3 points
+if x < y: 0 point
+if x = y: 1 point
+Notes:
+
+there are 10 matches in the championship
+0 <= x <= 4
+0 <= y <= 4
+*/
+
+//Program
+function points(games) {
+  let result = 0;
+  games.forEach(game => {
+    if (game[0] === game[2]) {
+      result += 1;
+    } else if (game[0] > game[2]) {
+      result += 3;
+    }
+  });
+  return result;
+}
+
+// Console testing
+console.log(points(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"]));
+
+// Expected output
+// 30
