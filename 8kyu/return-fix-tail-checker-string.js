adc2b16650c4ad915c5076331d56465e75201b57
Mauricio De La Cruz <58452509+delacrucen@users.noreply.github.com>
2022-07-05 16:58:53 -0400

    Add solution

---
+++ 8kyu/return-fix-tail-checker-string.js
@@ -0,0 +1,39 @@
+// Problem
+/*
+Some new animals have arrived at the zoo. The zoo keeper is concerned that perhaps the animals do not have the right tails. To help her, you must correct the broken function to make sure that the second argument (tail), is the same as the last letter of the first argument (body) - otherwise the tail wouldn't fit!
+
+If the tail is right return true, else return false.
+
+The arguments will always be strings, and normal letters.
+*/
+
+// OG Program
+/* 
+function correctTail(bod, tail ;
+  
+  sub = body.substr(bodylength-(tail.length)
+  
+  if sub = tail) ;
+    return true
+  }
+  else 
+    return false
+*/
+
+// Fixed Program
+function correctTail(body, tail){
+  let sub = body.substr(body.length-(tail.length));
+  if (sub == tail){
+    return true;
+  }else{
+    return false;    
+  } 
+};
+
+// Console testing
+console.log(correctTail("cat", "t"));
+console.log(correctTail("Dog", "D"));
+
+// Expected output
+// true
+// false
